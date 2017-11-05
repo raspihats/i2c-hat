876f3bf056f75c191ed6bcfba3999cc84953af3e
Florin Costa <florin.costa83@gmail.com>
2017-11-06 00:33:09 +0200

    IRQ output pin configured to Open Drain

--- Src/main.c
+++ Src/main.c
@@ -430,7 +430,7 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pin = IRQ_Pin;
   GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
   GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
-  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_OPENDRAIN;
   GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
   LL_GPIO_Init(IRQ_GPIO_Port, &GPIO_InitStruct);
 
